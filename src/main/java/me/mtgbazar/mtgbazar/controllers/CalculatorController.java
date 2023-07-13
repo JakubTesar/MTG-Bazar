1e84dc1705047bd109a590a93908aacd49845bd1
Jakub Tesař <tessi@post.cz>
2023-07-13 22:09:12 +0200

    neco neco fix callculator

--- src/main/java/me/mtgbazar/mtgbazar/controllers/CalculatorController.java
+++ src/main/java/me/mtgbazar/mtgbazar/controllers/CalculatorController.java
@@ -1,22 +1,32 @@
 package me.mtgbazar.mtgbazar.controllers;
 
-import ch.qos.logback.core.model.Model;
 import me.mtgbazar.mtgbazar.models.CalculatorDTO;
+import me.mtgbazar.mtgbazar.models.CalculatorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/calculator")
 public class CalculatorController {
+    @Autowired
+    private CalculatorService calculatorService;
     @GetMapping
-    public String renderCalculator(@ModelAttribute CalculatorDTO calculatorDTO) {
+    public String renderCalculator(
+            @ModelAttribute CalculatorDTO calculatorDTO
+    ) {
         return "calculator";
     }
-
     @PostMapping
     public String calculate(@ModelAttribute CalculatorDTO calculatorDTO,
                             Model model){
+        float result = calculatorService.calculate(calculatorDTO);
+        model.addAttribute("result", result);
+
         return "result";
     }
 }
---
+++ src/main/java/me/mtgbazar/mtgbazar/models/CalculatorService.java
@@ -0,0 +1,23 @@
+package me.mtgbazar.mtgbazar.models;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+
+    public float calculate(CalculatorDTO calculator){
+        if (calculator.getOperation().equals("+")) {
+            return calculator.getNumber1() + calculator.getNumber2();
+        } else if (calculator.getOperation().equals("-")) {
+            return calculator.getNumber1() - calculator.getNumber2();
+        } else if ((calculator.getOperation().equals("*"))){
+            return calculator.getNumber1() * calculator.getNumber2();
+        } else { // Poslední možnost může být pouze dělení
+            if(calculator.getNumber2() == 0){
+                throw new IllegalArgumentException("Second number cannot be 0!");
+            }
+            return calculator.getNumber1() / calculator.getNumber2();
+        }
+    }
+
+}
