77383110027d4b2778c5fa6e07b7a6e036c61d72
Jakub Tesař <tessi@post.cz>
2023-11-14 20:26:10 +0100

    vylepšení deailUsera

--- src/main/java/me/mtgbazar/mtgbazar/configuration/ApplicationSecurityConfiguration.java
+++ src/main/java/me/mtgbazar/mtgbazar/configuration/ApplicationSecurityConfiguration.java
@@ -33,29 +33,3 @@ public class ApplicationSecurityConfiguration {
         return new BCryptPasswordEncoder();
     }
 }
-
-
-
-/*.authorizeHttpRequests()
-                    .requestMatchers("/articles/create", "/articles/edit/**", "/articles/delete/**")
-                        .authenticated() // Zde později budeme řešit role (autorizaci)
-                    .requestMatchers(
-                        "/styles/**", "/images/**", "/scripts/**", "/fonts/**",
-                        "/articles", "/", "/skills", "/references", "/contact",
-                        "/account/register", "/articles/**"
-                    )
-                        .permitAll()
-                    .anyRequest() // Ostatní stránky jako např. `/articles/**` budou pouze pro přihlášené uživatele
-                        .authenticated()
-                    .and()
-                .formLogin() // Pokud uživatel není přihlášen, přesměrujeme ho na login formulář
-                    .loginPage("/account/login") // Přihlašovací URL adresa
-                    .loginProcessingUrl("/account/login") // Přihlašovací URL adresa
-                    .defaultSuccessUrl("/articles", true) // Nastavení přesměrování po úspěšném přihlášení
-                    .usernameParameter("email") // Chceme se přihlašovat pomocí emailu
-                    .permitAll() // Povolit vstup na `/account/login` i nepřihlášeným uživatelům
-                    .and()
-                .logout()
-                    .logoutUrl("/account/logout") // Odhlašovací URL adresa
-                    .and()
-                .build();*/
