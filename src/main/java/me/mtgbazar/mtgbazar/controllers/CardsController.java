042d77a7947b44a4daed3125b16f61723c12793e
Jakub Tesař <73097855+JakubTesar@users.noreply.github.com>
2024-02-13 12:58:14 +0100

    snad done

--- src/main/java/me/mtgbazar/mtgbazar/controllers/CardsController.java
+++ src/main/java/me/mtgbazar/mtgbazar/controllers/CardsController.java
@@ -13,8 +13,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
@@ -50,12 +54,25 @@ public class CardsController {
         return "cards/allCards";
     }
 
-    @GetMapping("createCard")
+    @GetMapping("upload")
     public String createCardsDBS() throws IOException {
+        return "cards/upload";
+    }
+    @GetMapping("createDBS")
+    public String dbsCreate() throws IOException {
         cardLoadService.getAllCardsCSV();
         return "cards/allCards";
     }
-
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public String submit(@RequestParam("file") MultipartFile file, ModelMap modelMap) throws IOException {
+        modelMap.addAttribute("file", file);
+            File path = new File("resources\\" + file.getOriginalFilename());
+            path.createNewFile();
+            FileOutputStream output = new FileOutputStream(path);
+            output.write(file.getBytes());
+            output.close();
+        return "redirect:../cards";
+    }
     @PostMapping("{cardId}")
     @Transactional
     public String addCard(@PathVariable long cardId) {
